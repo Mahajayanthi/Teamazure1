2b91687e745409e6b37fa8e81dbb6dcf4c0cdb65
Mahajayanthi <94959966+Mahajayanthi@users.noreply.github.com>
2022-10-18 15:26:15 +0530

    Update TeamazureApplication.java

--- src/main/java/com/example/Teamazure/Teamazure/TeamazureApplication.java
+++ src/main/java/com/example/Teamazure/Teamazure/TeamazureApplication.java
@@ -10,24 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamazureApplication {
 	@GetMapping("/azure")
 	public String message() {
-		return "Azure Web Apps Service - Build & Deployed Successfully using Azure Pipeline".;
-		//The main aim of the project is to deploy the same code in GCP and Azure platform.\r\n";
-// 				+ "\r\n"
+		return "Azure Web Apps Service - Build & Deployed Successfully using Azure Pipeline"
+		+"The main aim of the project is to deploy the same code in GCP and Azure platform.\r\n"
+				+ "\r\n"
 		
-// 				+ "Microsoft Azure(Maha & Jayanthi):\n"
+ 				+ "Microsoft Azure(Maha & Jayanthi):\n"
 				
-// 				+ "Microsoft Azure is a public cloud computing platform—with solutions including Infrastructure as a Service (IaaS), Platform as a Service (PaaS), and Software as a Service (SaaS) that can be used for services such as analytics, virtual computing, storage and networking . "
+ 				+ "Microsoft Azure is a public cloud computing platform—with solutions including Infrastructure as a Service (IaaS), Platform as a Service (PaaS), and Software as a Service (SaaS) that can be used for services such as analytics, virtual computing, storage and networking . "
 				
 				
-// 				+ "These services and resources include storing your \r\n"
-// 				+ "data and transforming it, depending on your requirements.\r\n"
-// 				+ "\r\n"
+				+ "These services and resources include storing your \r\n"
+ 				+ "data and transforming it, depending on your requirements.\r\n"
+				+ "\r\n"
 				
-// 				+ ""
-// 				+ "Google Cloud Platform(Kiruba & Juju):\r\n"
-// 				+ "Google Cloud Platform is a suite of public cloud computing services offered by Google.\n"
-// 				+ "The platform includes a range of hosted services for compute, storage and application development that run on Google hardware...";
-	}
+ 				+ ""
+				+ "Google Cloud Platform(Kiruba & Juju):\r\n"
+ 				+ "Google Cloud Platform is a suite of public cloud computing services offered by Google.\n"
+ 				+ "The platform includes a range of hosted services for compute, storage and application development that run on Google hardware...";
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TeamazureApplication.class, args);
